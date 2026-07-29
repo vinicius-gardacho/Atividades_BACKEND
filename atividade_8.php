2f0d644ae1d0ba9ca5722ed427f694c74c59a0fa
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-07-28 21:11:37 -0300

    atividade 8

---
+++ atividade_8.php
@@ -0,0 +1,15 @@
+<?php
+
+$memes = [
+    "Nazaré Confusa",
+    "Surprised Pikachu",
+    "Eu conheço esse cachorro",
+    "Que mira bobo?",
+    "Bora Bill"
+];
+
+echo "Meus Memes Favoritos:";
+
+foreach ($memes as $meme) {
+    echo $meme . "\n";
+}
