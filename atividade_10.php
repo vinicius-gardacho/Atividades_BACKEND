ff0eff1d173323b446d130d9c070bb33c4c28162
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-07-28 21:39:51 -0300

    atividade 10

---
+++ atividade_10.php
@@ -0,0 +1,17 @@
+<?php
+
+$perfil = [
+    "Nome" => "Vinicius",
+    "Idade" => 18,
+    "Cidade" => "Palmital",
+    "Tchola" => "Não"
+];
+
+echo "Informações do Perfil: \n";
+
+echo "Nome: " . $perfil["Nome"] . "\n";
+echo "Idade: " . $perfil["Idade"] . " anos\n";
+echo "Cidade: " . $perfil["Cidade"] . "\n";
+echo "Tchola: " . $perfil["Tchola"] . "\n";
+
+?>
